bd7f405bac42ebfc67eecd586ec8ce7e32521896
Shivon Talavera <t.shivon@yahoo.com>
2023-01-17 21:39:40 -0600

    Section 2 up to 2.2.2

---
+++ Section02/2.1/MarkDownPractice.md
@@ -0,0 +1,12 @@
+#Food Order List
+
+   1. *Pizza*
+   2. Chicken
+   3. Turkey
+
+
+##List of Countries
+
+   - **Japan**
+   - Iraq
+   -
---
+++ Section02/2.2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,23 @@
+// create a constant variable (const)
+const str ="Hello Everyone";
+// create a variable that can be reassigned (let)
+let age;
+// create three variables and assign them values of different data types (=)
+let myName ="Shivon"
+let nextAge = 30;
+let isCool = false;
+// print the types of two variables that reference two different data types (typeof)
+
+// TIP: console.log() prints a value in the console/terminal
+console.log()
+// change the value referenced by a variable (dynamic typing)
+console.log(iscool)
+isCool = 18;
+// print the type of the variable you just changed (typeof)
+console.log(typeof is cool);
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+let myName ="Nicole"
+
+console.log("Hello, my name is " + {add a name} + ", and I am learning " + {add a language} + ". I have been practicing for only " +  {add a number} + " weeks, so my status as a master is: " + {true or false} + ".");
